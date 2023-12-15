19e09c9706ab66caa80876efb54953cc0e8efee7
Phyllis Likimani <likiphyllis@yahoo.com>
2023-12-14 18:09:09 -0600

    add js logic to html

---
+++ notes/lesson10_app.js
@@ -0,0 +1,7 @@
+function handleCreatePost(){
+    document.getElementById("post-form-container").style.display = "block"
+    /* console.log("Clicked on create post button") */
+}
+function hidePostForm() {
+    document.getElementById("post-form-container").style.display = "none"
+}
